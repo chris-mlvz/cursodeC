db9fcd2071088852b149c095223ac2eb9f01a1e8
Christian MV <chris.mlvz@gmail.com>
2021-01-24 19:51:26 -0600

    Extern Variable Declarations

---
+++ externVariables.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+//variable Declarations
+extern int a, b, c;
+
+float f, g, h;
+
+
+int main()
+{
+// Variable definition
+int a, b, c;
+//Variable Inicialization
+    g = 1000.999999999;
+    f = 1.111111111;
+
+    h = g + f;
+
+    printf("Value of sum is: %f", h);
+    return 0;
+}
