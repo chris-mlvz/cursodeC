5a6e4bdf5dae307f61d354fc5424dba866b9d1b6
Christian MV <chris.mlvz@gmail.com>
2021-01-25 18:31:44 -0600

    GOTO

---
+++ controlFlujoC/Break/goto.c
@@ -0,0 +1,14 @@
+for (size_t i = 0; i < count; i++)
+{
+    for (size_t i = 0; i < count; i++)
+    {
+        for (size_t i = 0; i < count; i++)
+        {
+            if(panic)
+                goto errorHandler
+        }
+        
+    }
+    
+}
+errorHandler: //Todo el código para componer tu desastre
