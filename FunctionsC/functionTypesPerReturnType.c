f7a6f7c5451f68f4b0928123822ac58edc8ccd2b
Christian MV <chris.mlvz@gmail.com>
2021-01-25 20:02:56 -0600

    functionsTypes

---
+++ FunctionsC/functionTypesPerReturnType.c
@@ -0,0 +1,29 @@
+Funciones sin argumentos y sin valor de Retorno
+
+void functionName(); //Declaración
+
+functionName(); //Llamar la función
+
+void functionName() //Definición
+{
+    //logica
+}
+
+Funciones con argumentos pero sin valores a Retornar
+
+void functionName(float) //Declaracióm
+
+functionName(float); //Llamar la función
+ 
+void functionName(float) //Definición
+{
+    //logica
+}
+
+Función sin argumentos pero que si tienen valores a Retornar
+
+float functionName()
+
+Funciones con argumentos con return value
+
+float functionName(int, float);
